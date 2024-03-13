50e06d8a4f48209cf60473df5e08caff53ea8159
Salman Mallick <121571530+thecharizzard@users.noreply.github.com>
2024-03-13 15:32:38 -0400

    Basic Template

---
+++ SwiftUIBasics/SwiftUIBasics/SwiftUIBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicsApp.swift
+//  SwiftUIBasics
+//
+//  Created by Salman on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
